388493291b291ca33a882855f13c0b60ca9822c7
inkfarer <inkfarer@inkfarer.com>
2021-05-15 13:28:00 +0300

    Display twitter handles when showing casters on main graphic

--- graphics/scripts/main.js
+++ graphics/scripts/main.js
@@ -43,28 +43,40 @@ SBShown.on('change', newValue => {
 	}
 });
 
-// Caster names
+const castersElem = document.querySelector('div.castersWrapper > .castersBG');
+const castersShowTl = gsap.timeline();
 
 casters.on('change', newValue => {
-	let bg = document.querySelector('.castersBG');
-	bg.innerHTML = '';
+	let elemHtml = '';
+	const castersHeight = Object.keys(newValue).length * 40;
+	gsap.set('div.castersWrapper > .castersBG', {height: castersHeight});
 
 	Object.keys(newValue).forEach((item, index, arr) => {
-		const caster = newValue[item];
-		const elem = document.createElement('fitted-text');
-		const htmlText = `${caster.name} <span class="pronoun">${caster.pronouns}</span>`;
-		elem.setAttribute('text', htmlText);
-		elem.setAttribute('max-width', '230');
-		elem.setAttribute('align', 'left');
-		elem.setAttribute('useInnerHTML', '');
+		const element = newValue[item];
 
-		bg.appendChild(elem);
+		elemHtml += `
+		<div class="caster">
+			<div class="caster-name">
+				<fitted-text text="${element.name} <span class=&quot;pronoun&quot;>${element.pronouns}</span>" useInnerHTML max-width="235"></fitted-text>
+			</div>
+			<div class="caster-twitter">
+				<fitted-text text="${element.twitter} <span class=&quot;pronoun&quot;>${element.pronouns}</span>" useInnerHTML max-width="235"></fitted-text>
+			</div>
+		</div>`
 	});
+
+	castersElem.innerHTML = elemHtml;
 });
 
 // Caster name animation
 
-nodecg.listenFor('mainShowCasters', 'ipl-overlay-controls', () => {
-	gsap.fromTo('.castersWrapper', {opacity: 0, y: -15}, {duration: 0.35, y: 0, opacity: 1, ease: 'power2.out'});
-	gsap.to('.castersWrapper', {duration: 0.5, y: 15, opacity: 0, delay: 10.65, ease: 'power2.in'});
+nodecg.listenFor('mainShowCasters', DASHBOARD_BUNDLE_NAME, () => {
+	const showDuration = 20;
+	castersShowTl.add(gsap.set('.caster > .caster-twitter', {opacity: 0}))
+		.add(gsap.set('.caster > .caster-name', {opacity: 1}))
+		.add(gsap.fromTo('.castersWrapper', {opacity: 0, y: -15}, {duration: 0.35, y: 0, opacity: 1, ease: 'power2.out'}))
+		.add(gsap.to({}, {duration: showDuration}))
+		.add(gsap.to('.caster > .caster-twitter', {opacity: 1, duration: 0.35}), `-=${showDuration / 2}`)
+		.add(gsap.to('.caster > .caster-name', {opacity: 0, duration: 0.35}), `-=${showDuration / 2}`)
+		.add(gsap.to('.castersWrapper', {duration: 0.5, y: 15, opacity: 0, delay: 10.65, ease: 'power2.in'}));
 });
